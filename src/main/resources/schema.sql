22eca466c8dffd55ea543e83091cf1a43079df9a
Egorov Andrey <134514892+InfernalBeard@users.noreply.github.com>
2024-02-16 19:52:51 +0300

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,37 +1,37 @@
-create table if not exists netology.CUSTOMERS
+CREATE TABLE IF NOT EXISTS netology.CUSTOMERS
 (
-    id serial primary key,
-    name varchar not null,
-    surname varchar not null,
-    age int     not null,
-    phone_number varchar not null
+    id serial PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    surname VARCHAR NOT NULL,
+    age INT NOT NULL,
+    phone_number VARCHAR NOT NULL
 );
 
 
-create table if not exists netology.ORDERS
+CREATE TABLE IF NOT EXISTS netology.ORDERS
 (
-    id serial primary key,
-    date timestamp not null,
-    customer_id int,
-    product_name varchar not null,
-    amount int not null,
-    foreign key (customer_id) references netology.CUSTOMERS(id)
+    id serial PRIMARY KEY,
+    date TIMESTAMP NOT NULL,
+    customer_id INT,
+    product_name VARCHAR NOT NULL,
+    amount INT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES netology.CUSTOMERS(id)
 );
 
-insert into netology.CUSTOMERS (name, surname, age, phone_number)
-values ('TIMUR', 'DUSHANOV', 31, '+77077777777');
+INSERT INTO netology.CUSTOMERS (name, surname, age, phone_number)
+VALUES ('TIMUR', 'DUSHANOV', 31, '+77077777777');
 
-insert into netology.CUSTOMERS (name, surname, age, phone_number)
-values ('ANAR', 'GAFAROV', 31, '+71011111111');
+INSERT INTO netology.CUSTOMERS (name, surname, age, phone_number)
+VALUES ('ANAR', 'GAFAROV', 31, '+71011111111');
 
-insert into netology.CUSTOMERS (name, surname, age, phone_number)
-values ('ALEXEY', 'POPOVICH', 25, '+77777777777');
+INSERT INTO netology.CUSTOMERS (name, surname, age, phone_number)
+VALUES ('ALEXEY', 'POPOVICH', 25, '+77777777777');
 
-insert into netology.ORDERS (date, customer_id, product_name, amount)
+INSERT INTO netology.ORDERS (date, customer_id, product_name, amount)
 VALUES (current_date, 3, 'laptop', 2);
 
-insert into netology.ORDERS (date, customer_id, product_name, amount)
+INSERT INTO netology.ORDERS (date, customer_id, product_name, amount)
 VALUES (current_date, 3, 'iphone', 2);
 
-insert into netology.ORDERS (date, customer_id, product_name, amount)
+INSERT INTO netology.ORDERS (date, customer_id, product_name, amount)
 VALUES (current_date, 2, 'iphone', 2);
